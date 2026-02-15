6e441a6b73776094c69d85b837d8b5c502fb0de7
Hasitha <hasitha.karrunarathne@gmail.com>
2026-02-15 15:09:40 +0530

    bug fix

--- test_open_ai_key.py
+++ test_open_ai_key.py
@@ -1,7 +1,7 @@
 import os
 from openai import OpenAI
 
-key = "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"
+key = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 print("OPENAI_API_KEY set?", bool(key))
 
 client = OpenAI(api_key=key)
